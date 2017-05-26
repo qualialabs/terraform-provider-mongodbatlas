197fff9d3d0eb3073905357dc4dc707e2cd5cd63
Ling Zhang <zhangling.ice@gmail.com>
2017-05-26 11:45:58 -0700

    finish provider for Atlas mongodbuser

--- mongodbatlas/resource_mongodbuser.go
+++ mongodbatlas/resource_mongodbuser.go
@@ -1,11 +1,11 @@
 package mongodbatlas
 
 import (
-    // "log"
-    // "fmt"
+    "log"
+    "fmt"
     // "time"
     "github.com/hashicorp/terraform/helper/schema"
-    // "github.com/qualialabs/mongodbatlas-go-api/mongodbatlas"
+    "github.com/qualialabs/mongodbatlas-go-api/mongodbatlas"
 )
 
 func resourceMongodbUser() *schema.Resource {
@@ -23,6 +23,7 @@ func resourceMongodbUser() *schema.Resource {
             "db_user": &schema.Schema{
                 Type:     schema.TypeString,
                 Required: true,
+                ForceNew: true,
             },
             "db_password": &schema.Schema{
                 Type:     schema.TypeString,
@@ -36,26 +37,26 @@ func resourceMongodbUser() *schema.Resource {
 // #######################
 func resourceMongodbUserCreate(d *schema.ResourceData, meta interface{}) error {
 
-    // client := meta.(*mongodbatlas.Client)
-    // // create user
-    // db_name := d.Get("db_name").(string)
-    // db_user := d.Get("db_user").(string)
-    // db_password := d.Get("db_password").(string)
-    // log.Println("[DEBUG] Creating new user" + db_user +" on mongodb " + db_name )
+    client := meta.(*mongodbatlas.Client)
+    // create user
+    db_name := d.Get("db_name").(string)
+    db_user := d.Get("db_user").(string)
+    db_password := d.Get("db_password").(string)
+    log.Println("[DEBUG] Creating new user" + db_user +" on mongodb " + db_name )
 
-    // mongodbuser := &mongodbatlas.MongodbUser{
-    //   Username: db_user,
-    //   Password: db_password,
-    //   DatabaseName: db_name,
-    // }
-    // err := client.CreateMongodbUser(mongodbuser)
+    mongodbuser := &mongodbatlas.MongodbUser{
+      Username: db_user,
+      Password: db_password,
+      DatabaseName: db_name,
+    }
+    err := client.CreateMongodbUser(mongodbuser)
 
-    // if err != nil {
-    //   return fmt.Errorf("Failed to create mongodb user: %s", err.Error())
-    // }
+    if err != nil {
+      return fmt.Errorf("Failed to create mongodb user: %s", err.Error())
+    }
 
-    // d.SetId(db_name)
-    // log.Printf("[INFO] record ID: %s", d.Id())
+    d.SetId(db_user)
+    log.Printf("[INFO] record ID: %s", d.Id())
     
     return nil
 }
@@ -66,48 +67,48 @@ func resourceMongodbUserRead(d *schema.ResourceData, meta interface{}) error {
 }
 
 func resourceMongodbUserUpdate(d *schema.ResourceData, meta interface{}) error {
-    // client := meta.(*mongodbatlas.Client)
+    client := meta.(*mongodbatlas.Client)
 
-    // db_name := d.Get("db_name").(string)
-    // db_user := d.Get("db_user").(string)
-    // db_password := d.Get("db_password").(string)
-    // log.Println("[DEBUG] Updating user" + db_user +" on mongodb " + db_name )
+    db_name := d.Get("db_name").(string)
+    db_user := d.Get("db_user").(string)
+    db_password := d.Get("db_password").(string)
+    log.Println("[DEBUG] Updating user" + db_user +" on mongodb " + db_name )
 
-    // mongodbuser := &mongodbatlas.MongodbUser{
-    //   Username: db_user,
-    //   Password: db_password,
-    //   DatabaseName: db_name,
-    // }
-    // err := client.UpdateMongodbUser(mongodbuser)
+    mongodbuser := &mongodbatlas.MongodbUser{
+      Username: db_user,
+      Password: db_password,
+      DatabaseName: db_name,
+    }
+    err := client.UpdateMongodbUser(mongodbuser)
 
-    // if err != nil {
-    //   return fmt.Errorf("Failed to update mongodb user: %s", err.Error())
-    // }
+    if err != nil {
+      return fmt.Errorf("Failed to update mongodb user: %s", err.Error())
+    }
 
-    // d.SetId(db_name)
-    // log.Printf("[INFO] record ID: %s", d.Id())
+    d.SetId(db_name)
+    log.Printf("[INFO] record ID: %s", d.Id())
     
     return nil
 }
 
 func resourceMongodbUserDelete(d *schema.ResourceData, meta interface{}) error {
-    // client := meta.(*mongodbatlas.Client)
+    client := meta.(*mongodbatlas.Client)
 
-    // db_name := d.Get("db_name").(string)
-    // db_user := d.Get("db_user").(string)
-    // db_password := d.Get("db_password").(string)
-    // log.Println("[DEBUG] Deleting user" + db_user +" on mongodb " + db_name )
+    db_name := d.Get("db_name").(string)
+    db_user := d.Get("db_user").(string)
+    db_password := d.Get("db_password").(string)
+    log.Println("[DEBUG] Deleting user" + db_user +" on mongodb " + db_name )
 
-    // mongodbuser := &mongodbatlas.MongodbUser{
-    //   Username: db_user,
-    //   Password: db_password,
-    //   DatabaseName: db_name,
-    // }
-    // err := client.DeleteMongodbUser(mongodbuser)
+    mongodbuser := &mongodbatlas.MongodbUser{
+      Username: db_user,
+      Password: db_password,
+      DatabaseName: db_name,
+    }
+    err := client.DeleteMongodbUser(mongodbuser)
 
-    // if err != nil {
-    //   return fmt.Errorf("Failed to delete mongodb user: %s", err.Error())
-    // }
+    if err != nil {
+      return fmt.Errorf("Failed to delete mongodb user: %s", err.Error())
+    }
 
     return nil
 
